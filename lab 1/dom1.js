8450fb2df2c8f00c4aee4f7bb4d495dc893df2de
adityagola16 <golaaditya1234@gmail.com>
2026-07-30 11:47:30 +0530

    first commit

---
+++ lab 1/dom1.js
@@ -0,0 +1,8 @@
+// dom (document object model )
+import { EventEmitter } from "events";
+const button = new  EventEmitter();
+button.on ("click",() => {
+    console.log("Button clicked");
+
+});
+button.emit("click");
---
+++ lab 1/dom3.js
@@ -0,0 +1,10 @@
+// dom (document object model )
+import { EventEmitter } from "events";
+const button = new EventEmitter();
+button.on("click", (uname) => {
+  console.log(`button clicked by ${uname}`);
+});
+button.emit("click", "raju");
+button.emit("click", "kaju");
+button.emit("click", "rani");
+button.emit("click");
